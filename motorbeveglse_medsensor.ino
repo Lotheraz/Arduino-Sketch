cc2919932801e6144ff8514cb4ff4e5b9af5587f
Ove <100196459+Lotheraz@users.noreply.github.com>
2024-03-20 12:57:55 +0100

    Update motorbeveglse_medsensor.ino
    Added functionality for GUI referance

--- motorbeveglse_medsensor.ino
+++ motorbeveglse_medsensor.ino
@@ -13,41 +13,50 @@ void setup() {
 }
 
 void loop() {
-    int sensorValue = digitalRead(sensorDigitalPin); // Read the sensor value
-    digitalWrite(dirPin, HIGH);
-  while(digitalRead(sensorDigitalPin)==LOW){
-        
-        digitalWrite(stepPin, HIGH);
-        delayMicroseconds(500);
-        digitalWrite(stepPin, LOW);
-        delayMicroseconds(500);
-      
+    // Check if data is available to read
+    if (Serial.available() > 0) {
+        // Read the incoming string
+        String receivedString = Serial.readStringUntil('\n');
 
-  }
-    // If the sensor is triggered, stop the motor and prepare to move it down
-    if (sensorValue == HIGH) {
-        Serial.println("Limit reached, moving down");
-        moveMotor(false, 16000); // Reverse direction and move down
-    } else {
-        moveMotor(true, 200); // Keep moving up
+        // Check if the received string is "11"
+        if (receivedString == "11") {
+            moveMotorUpUntilSensorTriggered();
+            moveMotorDownAfterTrigger();
+        }
     }
-    delay(400);
+}
+
+void moveMotorUpUntilSensorTriggered() {
+    // Move up until the sensor is triggered
+    digitalWrite(dirPin, HIGH); // Set direction up
+    while(digitalRead(sensorDigitalPin) == LOW) {
+        // Sensor has not triggered yet, keep moving
+        digitalWrite(stepPin, HIGH);
+        Serial.println("Calibration in progress");
+        delayMicroseconds(1000);
+        digitalWrite(stepPin, LOW);
+        delayMicroseconds(1000);
+    }
+    Serial.println("Limit reached");
+}
+
+void moveMotorDownAfterTrigger() {
+    // Move down a certain number of steps after the sensor is triggered
+    Serial.println("Calibration in progress");
+    moveMotor(false, 16000); // Reverse direction and move down
 }
 
 void moveMotor(bool directionUp, int steps) {
     digitalWrite(enPin, LOW); // Enable the motor
-    digitalWrite(dirPin, directionUp ? HIGH : LOW); // Set directionm
+    digitalWrite(dirPin, directionUp ? HIGH : LOW); // Set direction
 
     // Perform steps
-    int x = 0;
-    while (x < steps) {
+    for (int x = 0; x < steps; x++) {
         digitalWrite(stepPin, HIGH);
-        delayMicroseconds(500);
+        delayMicroseconds(1000);
         digitalWrite(stepPin, LOW);
-        delayMicroseconds(500);
-        x++;
+        delayMicroseconds(1000);
     }
-
     // Optionally, you can disable the motor after the move
-    // digitalWrite(enPin, HIGH);
+    // digitalWrite(enPin, HIGH); // Disable the motor
 }
