a52e445ee31d07d12e1a230c88a4e83d3596e6f1
89Omar <92883272+89Omar@users.noreply.github.com>
2024-05-20 21:50:56 +0200

    Update sugepumpe_sketch2.ino

--- sugepumpe_sketch2.ino
+++ sugepumpe_sketch2.ino
@@ -1,37 +1,34 @@
-const int dirPin =7;
-const int stepPin =6;
-const int enPin = 5;
-
+const int dirPin = 7;  // Direction pin for the stepper motor
+const int stepPin = 6; // Step pin for the stepper motor
+const int enPin = 5;   // Enable pin for the stepper motor
 
 void setup() {
-  pinMode(stepPin, OUTPUT);
-  pinMode(dirPin, OUTPUT);
-  pinMode(enPin, OUTPUT);
-  digitalWrite(enPin, LOW);
-  Serial.begin(9600);
-
+  pinMode(stepPin, OUTPUT); // Set step pin as an output
+  pinMode(dirPin, OUTPUT);  // Set direction pin as an output
+  pinMode(enPin, OUTPUT);   // Set enable pin as an output
+  digitalWrite(enPin, LOW); // Enable the motor
+  Serial.begin(9600);       // Initialize serial communication
 }
 
 void loop() {
-    
-  sugePumpe(7,6,5,6000);
+  sugePumpe(7, 6, 5, 6000); // Call function to run the pump for 60 seconds
 }
-void sugePumpe(int dirPin, int stepPin, int endPin, unsigned long tid){
-   // Aktiverer pumpen (sender kontinuerlige stegsignaler) i 60 sekunder
-  unsigned long startTime = millis(); // Lagrer starttiden
-  digitalWrite(dirPin, HIGH);
-  while (millis() - startTime < 60000) { // Så lenge det ikke har gått 60 sekunder
-    // Sender et stegsignal
-    digitalWrite(dirPin, HIGH); // Sett retning til å suge
-    digitalWrite(stepPin, HIGH);
-    delayMicroseconds(500); // Du kan endre denne for å justere steghastigheten
-    digitalWrite(stepPin, LOW);
-    delayMicroseconds(500); // Du kan endre denne for å justere steghastigheten
-     }
-  
-  // Slår av pumpen etter 60 sekunder
-  digitalWrite(enPin, HIGH); // Slår av motoren
 
-  // Stopper loopen, slik at koden ikke kjører igjen
+void sugePumpe(int dirPin, int stepPin, int enPin, unsigned long tid) {
+  // Activate the pump (send continuous step signals) for 60 seconds
+  unsigned long startTime = millis(); // Store the start time
+  digitalWrite(dirPin, HIGH); // Set direction to suck
+  while (millis() - startTime < 60000) { // As long as 60 seconds have not passed
+    // Send a step signal
+    digitalWrite(stepPin, HIGH);
+    delayMicroseconds(500); // Adjust this to change the step speed
+    digitalWrite(stepPin, LOW);
+    delayMicroseconds(500); // Adjust this to change the step speed
+  }
+
+  // Turn off the pump after 60 seconds
+  digitalWrite(enPin, HIGH); // Disable the motor
+
+  // Stop the loop, so the code does not run again
   while (true) {}
 }
